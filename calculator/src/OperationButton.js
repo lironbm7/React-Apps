bfe30d8475efabaa43450a57f098e308c5fb9c44
Liron Biam <lironbm27@gmail.com>
2022-12-05 15:07:43 +0200

    Initial commit, skeleton done with 2 functions

---
+++ calculator/src/OperationButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from './App.js';
+
+export default function OperationButton( { dispatch, operation }) {
+  return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}>{operation}</button>
+}
